40a92cb6017eae339744f686d23b2436c50a6835
wuyuxin <876465382@qq.com>
2019-09-17 20:21:42 +0800

    feat: initialize project

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveForward = Vector2.zero;
+        moveForward.x = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().MovePosition(Quaternion.LookRotation(transform.forward) * moveForward * moveSpeed * Time.fixedDeltaTime + transform.position);
+    }
+}
