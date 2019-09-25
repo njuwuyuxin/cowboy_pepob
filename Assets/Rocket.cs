25902b5b019fa71afc05790f2bba6619477c4a40
XuShengTao <sheentxu@foxmail.com>
2019-09-25 18:18:32 +0800

    增加了子弹射击的实践
    从这里学到的 https://www.youtube.com/watch?v=_Z1t7MNk0c4

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+        Vector3 v = transform.position- player.position ;
+        v.z = 0;
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.left, v);
+        transform.rotation = rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y) {
+            DestoryProjectile();
+        }
+    }
+
+    void DestoryProjectile() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BirdEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdEnemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
+
+        //}
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+    }
+}
