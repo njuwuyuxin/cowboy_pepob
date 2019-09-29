06db6f423fbcc0a0d45d7dca1fb868c8e56f336a
XuShengTao <sheentxu@foxmail.com>
2019-09-29 17:50:46 +0800

    增加脚本寻路
    在脚本中寻路，为之后实现巡逻警戒追踪三态搭框架

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWayPointDistance = 3f;
+
+    public Transform enemyGFX;
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath=false;
+    Seeker seeker;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        //重复寻路/0.5s
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+    void UpdatePath() {
+        if(seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p) {
+        if (!p.error) {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null) {
+            return;
+        }
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+        }
+        else {
+            reachedEndOfPath = false;
+        }
+        //得到施加力的方向，记得加线性阻力，否则不会停
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+        if (distance < nextWayPointDistance) {
+            currentWayPoint++;
+        }
+        //转身
+        if (force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
