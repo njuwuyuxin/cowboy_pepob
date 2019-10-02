1b558d06b0528b7c78f73fc9fc01ca6e041f7d54
wuyuxin <876465382@qq.com>
2019-10-02 21:10:16 +0800

    feat:增加摄像机追踪与边界检测，简易场景搭建

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform FollowObject;
+    public float BorderLeft=0;
+    public float BorderRight=10000;
+    public float BorderBottom=0;
+    public float BorderTop=4;
+    private Transform CameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraTransform =  GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FollowObject.position.x < BorderLeft)
+        {
+            CameraTransform.position = new Vector3(BorderLeft, CameraTransform.position.y,-10f);
+        }
+        else if (FollowObject.position.x > BorderRight)
+        {
+            CameraTransform.position = new Vector3(BorderRight, CameraTransform.position.y, -10f);
+        }
+        else
+        {
+            CameraTransform.position = new Vector3(FollowObject.position.x, CameraTransform.position.y,-10f);
+        }
+
+        if (FollowObject.position.y < BorderBottom)
+        {
+            CameraTransform.position = new Vector3(CameraTransform.position.x, BorderBottom, -10f);
+        }
+        else if (FollowObject.position.y > BorderTop)
+        {
+            CameraTransform.position = new Vector3(CameraTransform.position.x, BorderTop, -10f);
+        }
+        else
+        {
+            CameraTransform.position = new Vector3(CameraTransform.position.x, FollowObject.position.y, -10f);
+        }
+    }
+}
