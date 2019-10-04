d92fe8be27a8bdc5f2c1f5fe7698b37d2c5cde19
nietzstl <1054893827@qq.com>
2019-10-04 22:07:36 +0800

    初步的射击

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,34 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Rocket : MonoBehaviour
+public class Rocket : MonoBehaviour 
 {
-    public float speed;
+	public GameObject explosion;		// Prefab of explosion effect.
 
-    private Transform player;
-    private Vector2 target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        target = new Vector2(player.position.x, player.position.y);
-        Vector3 v = transform.position- player.position ;
-        v.z = 0;
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.left, v);
-        transform.rotation = rotation;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        if (transform.position.x == target.x && transform.position.y == target.y) {
-            DestoryProjectile();
-        }
-    }
+	void Start () 
+	{
+		// Destroy the rocket after 2 seconds if it doesn't get destroyed before then.
+		Destroy(gameObject, 2);
+	}
 
-    void DestoryProjectile() {
-        Destroy(gameObject);
-    }
+
+	void OnExplode()
+	{
+		// Create a quaternion with a random rotation in the z-axis.
+		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+
+		// Instantiate the explosion where the rocket is with the random rotation.
+		Instantiate(explosion, transform.position, randomRotation);
+	}
+	
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		// If it hits an enemy...
+		if(col.tag == "Enemy")
+		{
+			// ... find the Enemy script and call the Hurt function.
+			col.gameObject.GetComponent<Enemy>().Hurt();
+
+			// Call the explosion instantiation.
+			OnExplode();
+
+			// Destroy the rocket.
+			Destroy (gameObject);
+		}
+		// Otherwise if it hits a bomb crate...
+		else if(col.tag == "BombPickup")
+		{
+			// ... find the Bomb script and call the Explode function.
+			col.gameObject.GetComponent<Bomb>().Explode();
+
+			// Destroy the bomb crate.
+			Destroy (col.transform.root.gameObject);
+
+			// Destroy the rocket.
+			Destroy (gameObject);
+		}
+		// Otherwise if the player manages to shoot himself...
+		else if(col.gameObject.tag != "Player")
+		{
+			// Instantiate the explosion and destroy the rocket.
+			OnExplode();
+			Destroy (gameObject);
+		}
+	}
 }
---
+++ Assets/Scripts/gun_fix.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+public class gun_fix : MonoBehaviour
+{
+    public Rigidbody2D rocket;              // Prefab of the rocket.
+    public float speed = 20f;               // The speed the rocket will fire at.
+    Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+    RaycastHit shootHit;                               // A raycast hit to get information about what was hit
+    private PlayerControl playerCtrl;       // Reference to the PlayerControl script.
+    private Animator anim;                  // Reference to the Animator component.
+    int shootableMask;
+    LineRenderer gunLine;
+    int range = 100;
+    int smothing;
+    GameObject player;
+    void Awake()
+    {
+        // Setting up the references.
+        shootableMask = LayerMask.GetMask("Shootable");
+        player= GameObject.FindGameObjectWithTag("Player");
+        gunLine = GetComponent<LineRenderer>();
+        //anim = transform.root.gameObject.GetComponent<Animator>();
+        playerCtrl = transform.root.GetComponent<PlayerControl>();
+        
+    }
+
+
+    void Update()
+    {
+        Vector3 start = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+        Vector3 end = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        gunLine.SetPosition(0, start);
+        gunLine.SetPosition(1, end);
+        // If the fire button is pressed...
+       
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            //anim.SetTrigger("Shoot");
+            //GetComponent<AudioSource>().Play();
+            //Vector3 start = Camera.main.ScreenToWorldPoint(new Vector3(player.transform.position.x, player.transform.position.y,0));
+            
+            //Vector3 end = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,0));
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = Vector2.Angle(new Vector2(pos.x,pos.y), new Vector2(1, 0));
+            start.z = 0;
+            end.z = 0;
+            if (pos.y < 0)
+                angle = -angle;
+            angle = angle  *Mathf.Deg2Rad;
+            //shootRay.origin = transform.position;
+            //shootRay.direction = pos;
+            //Physics.Raycast(shootRay, out shootHit, range, shootableMask);
+            //gunLine.SetPosition(0, start);
+            //gunLine.SetPosition(1, end);
+            // If the player is facing right...
+           
+                // ... instantiate the rocket facing right and set it's velocity to the right. 
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(speed*Mathf.Cos(angle), speed*Mathf.Sin(angle));
+           
+        }
+    }
+}
