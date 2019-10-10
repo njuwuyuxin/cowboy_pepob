10d046a78e2d40fb48f3a8d907a17d6366c35afc
XuShengTao <sheentxu@foxmail.com>
2019-10-11 01:24:54 +0800

    增加巡逻模式

--- Assets/Scripts/BirdEnemy.cs
+++ Assets/Scripts/BirdEnemy.cs
@@ -6,36 +6,45 @@ public class BirdEnemy : MonoBehaviour
 {
     public float speed;
     public float stoppingDistance;
-    public float retreatDistance;
 
     private float timeBtwShots;
     public float startTimeBtwShots;
 
     public GameObject projectile;
     public Transform player;
-
+    private bool canFire = false;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timeBtwShots = startTimeBtwShots;
     }
-
+    void startFire() {
+        canFire = true;
+    }
+    void stopFire()
+    {
+        canFire = false;
+    }
     // Update is called once per frame
     void Update()
     {
         //if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
 
         //}
-        if (timeBtwShots <= 0)
-        {
-            //发射子弹
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
+        if (canFire) {
+            if (timeBtwShots <= 0)
+            {
+                //发射子弹
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
         }
-        else {
-            timeBtwShots -= Time.deltaTime;
-        }
+
 
     }
 }
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,8 +11,11 @@ public class EnemyAI : MonoBehaviour
     private EnemyState state = EnemyState.PATROL;
     public float speed = 200f;
     public float nextWayPointDistance = 3f;
-
+    public float trackSecond = 5f;
     public Transform enemyGFX;
+    public GameObject eneity;
+    public Vector2 patrolStart;
+    public Vector2 patrolEnd;
     Path path;
     int currentWayPoint = 0;
     bool reachedEndOfPath=false;
@@ -34,14 +37,19 @@ public class EnemyAI : MonoBehaviour
     //朝向
     private Vector3 forward = new Vector3(-1f, 0f, 0f);
     private Vector3 up = new Vector3(0f, 0f, 1f);
+    private Color sightColor = Color.green;
+    private bool isToEnd = true;
     void OnDrawGizmos()
     {
         Color color = Handles.color;
-        Handles.color = Color.red;
+        Handles.color = sightColor;
         int angle = viewAngle / 2;
         //绕z轴旋转半个
         Vector3 startLine = Quaternion.Euler(0,0 , -angle) * forward;
         Handles.DrawSolidArc(this.enemyGFX.position, up, new Vector3(startLine.x, startLine.y, 0f), viewAngle, viewRadius);
+        Handles.color = Color.blue;
+        Handles.DrawSolidDisc(new Vector3(patrolStart.x, patrolStart.y,-1), up,0.3f);
+        Handles.DrawSolidDisc(new Vector3(patrolEnd.x, patrolEnd.y, -1), up, 0.3f);
         Handles.color = color;
     }
     float getAngle(Vector3 fromVector, Vector3 toVector) {
@@ -51,8 +59,22 @@ public class EnemyAI : MonoBehaviour
         return angle;
     }
     void UpdatePath() {
-        if(state != EnemyState.PATROL && seeker.IsDone())
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        if (seeker.IsDone()) {
+            if (state == EnemyState.PATROL) {
+                if (isToEnd)
+                {
+                    seeker.StartPath(rb.position, new Vector3(patrolEnd.x, patrolEnd.y, -1), OnPathComplete);
+                }
+                else {
+                    seeker.StartPath(rb.position, new Vector3(patrolStart.x, patrolStart.y, -1), OnPathComplete);
+                }
+            }else{
+                seeker.StartPath(rb.position, target.position, OnPathComplete);
+            }
+            
+        }
+        
+            
     }
     void OnPathComplete(Path p) {
         if (!p.error) {
@@ -66,7 +88,6 @@ public class EnemyAI : MonoBehaviour
     {
         switch (state) {
             case EnemyState.PATROL:
-                Debug.Log("PATROL");
                 float rangerSqr = (this.enemyGFX.position - target.position).sqrMagnitude;
                 //距离小于视野
                 if (rangerSqr <= viewRadius * viewRadius)
@@ -76,42 +97,40 @@ public class EnemyAI : MonoBehaviour
                     //在视野里或者实在太近
                     if (angle < viewAngle / 2 && angle > -viewAngle / 2 || rangerSqr<= viewRadius) {
                         lastStartTrack = 0f;
+                        sightColor = Color.red;
                         state = EnemyState.TRACK;
+                        //暂时追踪模式疯狂开火
+                        eneity.SendMessage("startFire", "");
                     }
+                }
+                //xunluo
+                if (track(speed/2)) {
+                    isToEnd = !isToEnd;
                 }
                 break;
             case EnemyState.TRACK:
-                Debug.Log("TRACK");
-                track();
+                track(speed);
                 //至少追踪五秒后，并且离开根号2 *视野后停止最终
-                if ((lastStartTrack>5f && (this.enemyGFX.position - target.position).sqrMagnitude >= 2 * viewRadius * viewRadius)) {
+                if ((lastStartTrack> trackSecond && (this.enemyGFX.position - target.position).sqrMagnitude >= 2 * viewRadius * viewRadius)) {
+                    sightColor = Color.green;
                     state = EnemyState.PATROL;
+                    //巡逻模式停止开火
+                    eneity.SendMessage("stopFire", "");
                 }
                 break;
             case EnemyState.FIRE:
                 break;
         }
-
-
-
-
-
     }
 
     float lastStartTrack = 0;
-    void track()
+    bool track(float speed)
     {
-        if (path == null)
-        {
-            return;
+        if (path == null ){
+            return false;
         }
-        if (currentWayPoint >= path.vectorPath.Count)
-        {
-            reachedEndOfPath = true;
-        }
-        else
-        {
-            reachedEndOfPath = false;
+        if (currentWayPoint >= path.vectorPath.Count) {
+            return true;
         }
         //得到施加力的方向，记得加线性阻力，否则不会停
         Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
@@ -124,6 +143,7 @@ public class EnemyAI : MonoBehaviour
         {
             currentWayPoint++;
         }
+        Debug.Log(rb.velocity.magnitude);
         //转身
         if (force.x >= 0.01f)
         {
@@ -132,11 +152,13 @@ public class EnemyAI : MonoBehaviour
 
 
         }
+        
         else if (force.x <= -0.01f)
         {
             enemyGFX.localScale = new Vector3(1f, 1f, 1f);
             forward = new Vector3(-1f, 0f, 0f);
         }
         rb.AddForce(force);
+        return false;
     }
 }
