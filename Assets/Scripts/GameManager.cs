78ac5e714200445095d578d403cc82194254747d
wuyuxin <876465382@qq.com>
2019-10-15 21:15:04 +0800

    feat:增加GameManager基本框架

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager _GameManager = null; //单例模式
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_GameManager == null)
+            _GameManager = this;
+        else if (_GameManager != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadSceneByName(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int PlayerHP = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hurt(int damage)
+    {
+        PlayerHP -= damage;
+        if (PlayerHP <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+
+    }
+}
