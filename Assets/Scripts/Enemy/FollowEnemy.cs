a653df69557e7048c3e6f7ecbbed5aa743a5adb3
XuShengTao <sheentxu@foxmail.com>
2019-11-04 19:02:34 +0800

    add 警戒开火进度条

---
+++ Assets/Scripts/Enemy/FollowEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowEnemy : MonoBehaviour
+{
+	public Vector3 offset;			// The offset at which the Health Bar follows the player.
+	void Awake ()
+	{
+	}
+
+	void Update ()
+	{
+	}
+}
