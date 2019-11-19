57d6e8b588d9747b0639b13a298e3cc5fc373e05
wuyuxin <876465382@qq.com>
2019-11-19 17:31:03 +0800

    feat:增加打开宝箱时的UI

--- Assets/Scripts/GetBullets.cs
+++ Assets/Scripts/GetBullets.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Bullettype { Pistol,Blood, Assult };
 
@@ -9,6 +10,7 @@ public class GetBullets : MonoBehaviour
     public Bullettype BulletType;
     public int BulletCount;     //宝箱中藏有的子弹数量
     public bool PartialRandom=false;      //开启后会在原有子弹数量上随机小幅度波动，但不会过分随机
+    public GameObject InteractiveUI;                //显示宝箱UI
 
     private bool used = false;
     // Start is called before the first frame update
@@ -29,8 +31,10 @@ public class GetBullets : MonoBehaviour
         if (used)
             return;
         Debug.Log("触碰子弹宝箱");
+
         shoot shootScript = col.GetComponent<shoot>();
         Debug.Log(shootScript);
+        string UItext="";
 
         int count = BulletCount;
         if (PartialRandom)
@@ -40,16 +44,28 @@ public class GetBullets : MonoBehaviour
         if (BulletType == Bullettype.Pistol)
         {
             shootScript.Guns[0].bulletStore += count;
+            UItext = "获得手枪子弹x" + count;
         }
         if (BulletType == Bullettype.Blood)
         {
             shootScript.Guns[1].bulletStore += count;
+            UItext = "获得吸血枪子弹x" + count;
         }
         if (BulletType == Bullettype.Assult)
         {
             shootScript.Guns[2].bulletStore += count;
+            UItext = "获得突击步枪子弹x" + count;
         }
         shootScript.UpdateBulletCountUI();
+
+        InteractiveUI.SetActive(true);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Transform temp2 = InteractiveUI.transform.GetChild(0);
+        Vector2 offset = new Vector2(60, 25);
+        temp2.position = screenPos + offset;
+        temp2.gameObject.GetComponent<Text>().text = UItext;
+        Destroy(InteractiveUI, 2);
+
         used = true;
     }
 }
--- Assets/Scripts/GetGuns.cs
+++ Assets/Scripts/GetGuns.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public enum Guntype {Blood,Assult};
 public class GetGuns : MonoBehaviour
 {
     public Guntype GunType;
+    public GameObject InteractiveUI;                //显示宝箱UI
+    private bool used=false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,18 +23,33 @@ public class GetGuns : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D col)
     {
+        if (used)
+            return;
         if (col.tag != "Player")
             return;
         Debug.Log("触碰枪支宝箱");
-        PlayerManager manager = col.GetComponent<PlayerManager>();
 
+        string UItext = "";
+        PlayerManager manager = col.GetComponent<PlayerManager>();
         if (GunType == Guntype.Blood)
         {
             manager.Gun2Lock = true;
+            UItext = "获得 吸血枪";
         }
         if (GunType == Guntype.Assult)
         {
             manager.Gun3Lock = true;
+            UItext = "获得 突击步枪";
         }
+
+        InteractiveUI.SetActive(true);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Transform temp2 = InteractiveUI.transform.GetChild(0);
+        Vector2 offset = new Vector2(60, 25);
+        temp2.position = screenPos + offset;
+        temp2.gameObject.GetComponent<Text>().text = UItext;
+        Destroy(InteractiveUI, 2);
+
+        used = true;
     }
 }
