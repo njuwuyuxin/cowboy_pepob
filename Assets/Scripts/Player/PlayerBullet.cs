c0a8e571cfd78a931d1495d073326dfbae375f61
wuyuxin <876465382@qq.com>
2019-11-19 22:32:52 +0800

    feat:单独提出主角发射子弹脚本PlayerBullet

---
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public int DamagePerShoot;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.tag);
+        if (col.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<EnemyHealth>().Hurt(DamagePerShoot);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
